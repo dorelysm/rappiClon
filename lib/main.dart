8355123481c2d568a7a6d1da0177304e1d79b9e7
dorelysm <martinezdorelys@gmail.com>
2022-06-08 23:09:03 -0500

    primer repositorio

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rappi clon',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      //appBar: AppBar(),
+
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: Row(
+              children: const <Widget>[
+              Icon(
+                Icons.add_location,
+                color: Colors.orange,
+              ),
+              TextButton(onPressed: null, child: Text('Dirección'))
+              ]
+            )
+          ),
+          Card(
+            child: Row(
+              children: const <Widget>[
+                //Agregar TextField
+                IconButton(onPressed: null, icon: Icon(Icons.search)),
+                IconButton(onPressed: null, icon: Icon(Icons.notifications))
+              ]
+            )
+            
+          ),
+          Card(
+            child: Row(
+              children: const <Widget>[
+                Expanded(child: Text('Secciones')),
+                TextButton(onPressed: null, child: Text('Ver todos'))
+              ]
+            )
+          ),
+          const Card(
+            child: Divider(
+              height: 5,
+              thickness: 1,
+              color: Colors.grey,
+            )
+          ),
+          
+          const Card(
+            //Agregar GridView
+          ),
+
+          Card(
+            child: Row(
+              children: const <Widget>[
+                ElevatedButton(onPressed: null, child: Text('RappiPay')),
+                ElevatedButton(onPressed: null, child: Text('CashBack')),
+              ]
+            )
+          ),
+
+          Card(
+            color: Colors.orange,
+            child: Column(
+              children: const <Widget>[
+                Text("Supermercados"),
+                //Agregar listView
+                //ListView(
+                //  children: const <Widget>[
+                //    Card(
+                //      child: Text('Supermercados')
+                //    )
+                //  ]
+                //),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('Ver todos los mercados')
+                  )
+              ]
+              )
+          )
+
+        ]
+      ),
+
+    floatingActionButton: FloatingActionButton(
+      child: const Icon(Icons.menu),
+      onPressed: () {},
+    ), 
+    
+    //drawer: Drawer(),
+    );
+  }
+}
---
+++ lib/restaurantes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: RestaurantsPage());
+  }
+}
+
+class RestaurantsPage extends StatefulWidget {
+  RestaurantsPage({Key? key}) : super(key: key);
+
+  @override
+  State<RestaurantsPage> createState() => _RestaurantsPageState();
+}
+
+class _RestaurantsPageState extends State<RestaurantsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
